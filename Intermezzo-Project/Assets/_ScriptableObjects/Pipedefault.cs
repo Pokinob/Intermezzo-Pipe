98ec48590ca380fee1c0182e38b3c2484ad6ba2c
Pokinob <d1041251021@student.untan.ac.id>
2026-01-29 21:07:09 +0700

    changes

---
+++ Intermezzo-Project/Assets/_ScriptableObjects/Pipedefault.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pipedefault", menuName = "Scriptable Objects/Pipedefault")]
+public class Pipedefault : ScriptableObject
+{
+    public int zeroOne;
+    public int oneZero;
+    public int zeroMon;
+    public int monZero;
+}
