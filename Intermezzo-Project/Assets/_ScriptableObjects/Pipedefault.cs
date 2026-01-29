85fc1b6b1faf3f11ffda487839ff1a0b3b2a2908
Pokinob <d1041251021@student.untan.ac.id>
2026-01-29 21:07:53 +0700

    changes

--- Intermezzo-Project/Assets/_ScriptableObjects/Pipedefault.cs
+++ Intermezzo-Project/Assets/_ScriptableObjects/Pipedefault.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Pipedefault", menuName = "Scriptable Objects/Pipedefault")]
 public class Pipedefault : ScriptableObject
 {
-    public int zeroOne;
-    public int oneZero;
-    public int zeroMon;
-    public int monZero;
+    public bool zeroOne;
 }
--- Intermezzo-Project/Assets/_Scripts/pipeScript.cs
+++ Intermezzo-Project/Assets/_Scripts/pipeScript.cs
@@ -3,14 +3,31 @@ using UnityEngine.InputSystem;
 
 public class pipeScript : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Pipedefault _pipeDefault;
     [SerializeField]
     private float[] rotations = { 0, 90, 180, 270 };
+    [SerializeField]
+    private bool[] pipeBool; 
 
     private void Start()
     {
         int rand=Random.Range(0,rotations.Length);
         transform.eulerAngles= new Vector3(0, 0, rotations[rand]);
+        makeBool(rand);
     }
+
+    private void makeBool(int rand)
+    {
+        if (rand == 0)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+
 
 }
