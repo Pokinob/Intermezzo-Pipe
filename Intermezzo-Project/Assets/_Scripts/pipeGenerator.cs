ab38b8f7f6bfabac5e1e9885b8104388c68bb73a
ArrayNone <73766707+ArrayNone@users.noreply.github.com>
2026-02-04 06:20:06 +0700

    Spawn document nodes

--- Intermezzo-Project/Assets/_Scripts/pipeGenerator.cs
+++ Intermezzo-Project/Assets/_Scripts/pipeGenerator.cs
@@ -1,17 +1,29 @@
+using System.Security.Principal;
 using UnityEngine;
 
 public class pipeGenerator : MonoBehaviour
 {
     [SerializeField]
     private Transform camPos;
+
     [SerializeField]
     private GameObject[] pipePrefab;
+    [SerializeField]
+    private GameObject powerNodePrefab;
+    [SerializeField]
+    private GameObject destinationNodePrefab;
+    [SerializeField]
+    private GameObject enemyNodePrefab;
+
     [SerializeField]
     private int width, height;
     [SerializeField]
     private Vector2 cellSize;
     [SerializeField]
     private Vector2 cellPositionOffset;
+
+    [SerializeField]
+    private Vector2[] powerNodePositions;
 
     private void Start()
     {
@@ -25,13 +37,31 @@ public class pipeGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                int rand = Random.Range(0, pipePrefab.Length);
-                var spawnPipe = Instantiate(pipePrefab[rand], new Vector3(
-                    (x * cellSize.x)+0.5f+cellPositionOffset.x, 
-                    (y * cellSize.y)+0.5f+cellPositionOffset.y), 
-                    Quaternion.identity
+                Vector3 nextPosition = new Vector3(
+                    (x * cellSize.x) + 0.5f + cellPositionOffset.x,
+                    (y * cellSize.y) + 0.5f + cellPositionOffset.y
                 );
-                spawnPipe.name = $"pipe {x}{y}";
+
+                bool isPowerNode = false;
+                foreach (Vector2 nodePositions in powerNodePositions)
+                {
+                    if (x == nodePositions.x && y == nodePositions.y)
+                    {
+                        isPowerNode = true;
+                        break;
+                    }
+                }
+
+                if (isPowerNode) 
+                {
+                    GameObject nextSpawn = Instantiate(powerNodePrefab, nextPosition, Quaternion.identity);
+                    nextSpawn.name = $"power {x}-{y}";
+                } else
+                {
+                    int rand = Random.Range(0, pipePrefab.Length);
+                    GameObject nextSpawn = Instantiate(pipePrefab[rand], nextPosition, Quaternion.identity);
+                    nextSpawn.name = $"pipe {x}-{y}";
+                }
             }
         }
         //camPos.transform.position = new Vector3((float)width / 2f + cellSize.x, (float)height / 2f + 0.5f, camPos.transform.position.z);
