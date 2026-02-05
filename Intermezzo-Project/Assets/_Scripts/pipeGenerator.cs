a0634ae0dfa3640985dae12810706fcd6fa72e4d
ArrayNone <73766707+ArrayNone@users.noreply.github.com>
2026-02-05 09:51:24 +0700

    Double location generation

--- Intermezzo-Project/Assets/_Scripts/pipeGenerator.cs
+++ Intermezzo-Project/Assets/_Scripts/pipeGenerator.cs
@@ -213,6 +213,7 @@ public class pipeGenerator : MonoBehaviour
         {
             yield return new WaitForSeconds(targetSpawnTimeSeconds);
             generateNodeAtRandomPosition("target", destinationNodePrefab);
+            generateNodeAtRandomPosition("target", destinationNodePrefab);
         }
     }
 }
